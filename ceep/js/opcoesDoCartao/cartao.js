3b43897c37eaff738422039114587e1174b74304
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2019-02-20 17:12:10 -0300

    usando o objeto window para definir proprieda com funções

--- ceep/js/opcoesDoCartao/cartao.js
+++ ceep/js/opcoesDoCartao/cartao.js
@@ -1,88 +1,92 @@
-let cartoes = document.querySelectorAll('.cartao')
-let contador = cartoes.length;
+(()=>{
+    'use strict';
 
-function criarCartao(objetoCartao) {
-
-    contador++;
-
-    const cartao = document.createElement('article');
-    cartao.classList.add('cartao');
-    cartao.tabIndex = 0;
-    cartao.id = `cartao_${contador}`;
-
-    cartao.style.backgroundColor = objetoCartao.cor;
-
-    cartao.innerHTML = ` <div class="opcoesDoCartao">
-            <button class="opcoesDoCartao-remove opcoesDoCartao-opcao" tabindex="0">
-              <svg><use xlink:href="#iconeRemover"></use></svg>
-            </button>
-  
-            <input type="radio" name="corDoCartao${contador}" value="#EBEF40" id="corPadrão-cartao${contador}" class="opcoesDoCartao-radioTipo" checked>
-            <label for="corPadrão-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
-              Padrão
-            </label>
-  
-            <input type="radio" name="corDoCartao${contador}" value="#F05450" id="corImportante-cartao${contador}" class="opcoesDoCartao-radioTipo">
-            <label for="corImportante-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
-              Importante
-            </label>
-  
-            <input type="radio" name="corDoCartao${contador}" value="#92C4EC" id="corTarefa-cartao${contador}" class="opcoesDoCartao-radioTipo">
-            <label for="corTarefa-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
-              Tarefa
-            </label>
-
-            <input type="radio" name="corDoCartao${contador}" value="#76EF40" id="corInspiração-cartao${contador}" class="opcoesDoCartao-radioTipo">
-            <label for="corInspiração-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
-              Inspiração
-            </label>
-          </div>
-          <p class="cartao-conteudo" contenteditable tabindex="0">${objetoCartao.conteudo}</p>`;
-
-    cartao.addEventListener('focusin', (evento) => {
-
-        cartao.classList.add('cartao--focado')
-    })
-    cartao.addEventListener('focusout', (evento) => {
-
-        cartao.classList.remove('cartao--focado')
-
-    })
-    cartao.addEventListener('change', function mudaCor(event) {
-
-
-        const elementoSelecionado = event.target
-        const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
-               if (isRadioTipo) {
-            cartao.style.backgroundColor = elementoSelecionado.value
-        }
-    })
-
-    cartao.addEventListener("keyup", function deixaClicarComEnter(event) {
-        if (event.target.classList.contains("opcoesDoCartao-opcao") && (event.key === 'Enter' || event.key === '')) {
-            event.target.click()
-            console.log(event.target)
-        }
-    })
-
-    cartao.addEventListener('click', (evento) => {
-        console.log(evento)
-        isTrash = evento.target.classList.contains('opcoesDoCartao-remove')
-        if (isTrash) {
-            cartao.classList.add("cartao--some");
-            cartao.addEventListener("transitionend", function () {
-                this.remove()
-            })
-        }
-
-    })
- 
-
-    document.querySelector('.mural').insertAdjacentElement('beforeend', cartao);
-
-    return cartao;
-
-}
+    let cartoes = document.querySelectorAll('.cartao')
+    let contador = cartoes.length;
+    
+    window.criarCartao = function(objetoCartao){    
+    
+        contador++;
+    
+        const cartao = document.createElement('article');
+        cartao.classList.add('cartao');
+        cartao.tabIndex = 0;
+        cartao.id = `cartao_${contador}`;
+    
+        cartao.style.backgroundColor = objetoCartao.cor;
+    
+        cartao.innerHTML = ` <div class="opcoesDoCartao">
+                <button class="opcoesDoCartao-remove opcoesDoCartao-opcao" tabindex="0">
+                  <svg><use xlink:href="#iconeRemover"></use></svg>
+                </button>
+      
+                <input type="radio" name="corDoCartao${contador}" value="#EBEF40" id="corPadrão-cartao${contador}" class="opcoesDoCartao-radioTipo" checked>
+                <label for="corPadrão-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #EBEF40;" tabindex="0">
+                  Padrão
+                </label>
+      
+                <input type="radio" name="corDoCartao${contador}" value="#F05450" id="corImportante-cartao${contador}" class="opcoesDoCartao-radioTipo">
+                <label for="corImportante-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #F05450;" tabindex="0">
+                  Importante
+                </label>
+      
+                <input type="radio" name="corDoCartao${contador}" value="#92C4EC" id="corTarefa-cartao${contador}" class="opcoesDoCartao-radioTipo">
+                <label for="corTarefa-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #92C4EC;" tabindex="0">
+                  Tarefa
+                </label>
+    
+                <input type="radio" name="corDoCartao${contador}" value="#76EF40" id="corInspiração-cartao${contador}" class="opcoesDoCartao-radioTipo">
+                <label for="corInspiração-cartao${contador}" class="opcoesDoCartao-tipo opcoesDoCartao-opcao" style="color: #76EF40;" tabindex="0">
+                  Inspiração
+                </label>
+              </div>
+              <p class="cartao-conteudo" contenteditable tabindex="0">${objetoCartao.conteudo}</p>`;
+    
+        cartao.addEventListener('focusin', (evento) => {
+    
+            cartao.classList.add('cartao--focado')
+        })
+        cartao.addEventListener('focusout', (evento) => {
+    
+            cartao.classList.remove('cartao--focado')
+    
+        })
+        cartao.addEventListener('change', function mudaCor(event) {
+    
+    
+            const elementoSelecionado = event.target
+            const isRadioTipo = elementoSelecionado.classList.contains('opcoesDoCartao-radioTipo')
+                   if (isRadioTipo) {
+                cartao.style.backgroundColor = elementoSelecionado.value
+            }
+        })
+    
+        cartao.addEventListener("keyup", function deixaClicarComEnter(event) {
+            if (event.target.classList.contains("opcoesDoCartao-opcao") && (event.key === 'Enter' || event.key === '')) {
+                event.target.click()
+                console.log(event.target)
+            }
+        })
+    
+        cartao.addEventListener('click', (evento) => {
+            console.log(evento)
+            isTrash = evento.target.classList.contains('opcoesDoCartao-remove')
+            if (isTrash) {
+                cartao.classList.add("cartao--some");
+                cartao.addEventListener("transitionend", function () {
+                    this.remove()
+                })
+            }
+    
+        })
+     
+    
+        document.querySelector('.mural').insertAdjacentElement('beforeend', cartao);
+    
+        return cartao;
+    
+    }
+})()
 
 /* 
 ;(function(){
